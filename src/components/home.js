2d6656a3889d1e0128a874a62a2c333ba4bca80a
TSW <sachintha@Sachintha.Sachintha>
2024-01-20 21:46:47 +0530

    App.js and index.html changed

---
+++ src/components/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home(){
+    return(
+        <div className="main-container">
+            <div className="topic">
+                <h1>SLSACHIBOY</h1>
+                <img src="" alt="profile picture"/>
+            </div>
+        </div>
+    )
+}
